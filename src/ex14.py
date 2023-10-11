2a1da7e6b5970d3fb03d8c7f210c4563296e9fc4
kdai-ekkalukw <66056099@kmitl.ac.th>
2023-10-11 11:34:23 +0700

    assignment python 10-0708
    test ex14

--- src/ex14.py
+++ src/ex14.py
@@ -3,7 +3,7 @@ Exercise 14
 """
 
 
-def average(num_list: list) -> float:
+def average(num_list) -> float:
     """
     Calculate the average of a list of numbers.
 
@@ -14,4 +14,7 @@ def average(num_list: list) -> float:
     - float: The average of the numbers in the list.
       If the list is empty, returns 0.
     """
-    pass
+    avg = 0
+    avg = sum(num_list)/len(num_list)
+    float(avg)
+    return avg
