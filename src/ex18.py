be115c6f2b6e1d55f4fd1e1a3922090f476d8b97
kdai-ekkalukw <66056099@kmitl.ac.th>
2023-10-11 12:14:31 +0700

    assignment python 10-0708
    test ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,20 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(qty, amt):
+    #logic is 8 free 1
+    if qty > 8:
+        free = qty // 8
+    else:
+        free = 0
+    cost = (qty - free) * amt
+    return cost
+
+def get_cost_of_coffee_2(qty, amt):
+    #logic is 8 free 1
+    if qty > 8:
+        free = qty // 8
+    else:
+        free = 0
+    cost = (qty - free) * amt
+    return cost
