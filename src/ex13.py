b37fa40176015e384740f6ba15dc8f80eddf8809
kdai-ekkalukw <66056099@kmitl.ac.th>
2023-10-11 11:18:14 +0700

    assignment python 10-0708
    test ex13

--- src/ex13.py
+++ src/ex13.py
@@ -3,7 +3,7 @@ Exercise 13
 """
 
 
-def calc_sum(params):
+def calc_sum(num_list):
     """
     Calculate the sum of a list of numbers.
 
@@ -13,10 +13,14 @@ def calc_sum(params):
     Returns:
     - int: The sum of all the numbers in the list.
     """
-    pass
+    if num_list != []:
+        sum_val = sum(num_list)
+    else:
+        sum_val = 0
+    return sum_val
 
 
-def calc_prod(params):
+def calc_prod(num_list):
     """
     Calculates the product of all the numbers in the given list.
 
@@ -26,4 +30,10 @@ def calc_prod(params):
     Returns:
         int: The product of all the numbers in the list.
     """
-    pass
+    prod = 1
+    if num_list != []:
+        for i in num_list:
+            prod = prod * i
+    else:
+        prod = 1
+    return prod
