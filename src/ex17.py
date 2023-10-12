4517abb06a04e27e4af2352ea9e6578c96e2844b
kdai-ekkalukw <66056099@kmitl.ac.th>
2023-10-12 22:38:39 +0700

    assignment python 10-0708
    test ex17

--- src/ex17.py
+++ src/ex17.py
@@ -4,7 +4,7 @@ Exercise 17
 import random
 
 
-def roll_dice(params):
+def roll_dice(num_of_dice):
     """
     Calculate the total sum of rolling a certain number of dice.
 
@@ -14,4 +14,11 @@ def roll_dice(params):
     Returns:
         int: The total sum of the dice rolls.
     """
-    pass
+    sum_dice = 0
+    if num_of_dice == 0:
+        return 0
+    else:
+
+        for i in range(1,num_of_dice + 1):
+            sum_dice = sum_dice + random.randint(1,6)
+        return sum_dice
